114430c092207a2d91dc727002ddef969eb41bef
David Carboni <david@carboni.io>
2016-12-16 02:04:14 +0000

    Lightly tweaked Liferay code so that it will run in isolation. Like a brain in a vat.

---
+++ src/main/java/com/liferay/portal/security/pwd/PwdEncryptorException.java
@@ -0,0 +1,10 @@
+package com.liferay.portal.security.pwd;
+
+/**
+ * Stubbing out this liferay exceptio.
+ */
+public class PwdEncryptorException extends Exception {
+    PwdEncryptorException(String message) {
+
+    }
+}
